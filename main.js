9db28403d6da581f71b6f011128b37dc7260b2c1
SKieronski <GitPW123!@#>
2022-01-03 16:19:57 -0600

    Added one more white key for clarity in the demo.

---
+++ main.js
@@ -0,0 +1,20 @@
+//Default game mode will be easy
+//Initialize global variables
+let seriesN = 1;
+
+//Setup array of key objects
+//Number of objects will be equal to the game mode selected
+//objects will have a key name, audio src, and randomly assigned color.
+
+//Setup keys as new buttons and add to empty div? Can just start with keys in the div and manipulate those.
+
+//add code to play a series of notes starting with 1 note
+//player will have to copy the series each time it is played
+//Need code logic to give the player time to press the right keys.
+//Once player presses the right keys, next series of notes will be played.
+//Is this where async timing comes into play? For giving the player a time limit.
+
+//add event listeners to each key
+//check if the right key was pressed
+//when the key is pressed it plays the associated audio.
+
