ec76b89e025474fdf18d46630d3ae468d4f21b33
SKieronski <GitPW123!@#>
2022-01-04 16:16:27 -0600

    Game works! added functions for the key series to be chosen and the key series' audio to be played. Added conditional logic to the keyboard event listeners that determines when the player selects the right key in the series.

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ let seriesN = 1; //number of notes to play in series
 let gameMode = "easy"; //game difficulty
 let score = 0; //how many series have successfully been completed
 const numKeys = 5; //total number of keys
+let chosenKeys = [];
+let checker = 0;
 
 //Setup array of key objects
 //Number of objects will be equal to the game mode selected
@@ -38,6 +40,7 @@ let keyFifth = {
     color: "blue"
 }
 
+//Array holding our key objects
 let myKeys = [keyFirst, keySecond, keyThird, keyFourth, keyFifth];
 
 //Setup keys as new buttons and add to empty div? Can just start with keys in the div and manipulate those.
@@ -48,13 +51,29 @@ let myKeys = [keyFirst, keySecond, keyThird, keyFourth, keyFifth];
 //Once player presses the right keys, next series of notes will be played.
 //Is this where async timing comes into play? For giving the player a time limit.
 function playKey(key) {
-    // console.log("yo");
     let note = new Audio(key.audioSrc);
     note.play();
 }
 
-function test() {
-    console.log("yo");
+//Play all the keys in the chosenKeys array
+//This will need to change the color of the keys too and needs async functionality
+function playKeySeries() {
+    for(let i = 0; i < chosenKeys.length; i++) {
+        let note = new Audio(chosenKeys[i].audioSrc);
+        note.play();
+    }
+}
+
+//Change the color of a specific key document element to the key object's color value
+function changeKeyColor(docKey, myKey) {
+    docKey.style.backgroundColor = myKey.color;
+}
+
+//Add a key to the series challenge
+function chooseKey() {
+    let rand = Math.floor(Math.random() * 5);
+    chosenKeys.push(myKeys[rand]);
+    console.log(chosenKeys);
 }
 
 //add event listeners to each key
@@ -62,11 +81,33 @@ function test() {
 //when the key is pressed it plays the associated audio.
 let docKeys = document.getElementsByClassName("keyboard");
 for(let i = 0; i < docKeys.length; i++) {
-    console.log("in for loop");
     docKeys[i].addEventListener("click", () => {
         playKey(myKeys[i]);
+        if(myKeys[i].name === chosenKeys[checker].name) {
+            if(checker === chosenKeys.length - 1) { //the player beat that series so update the score and add more keys to the challenge list
+                checker = 0;
+                score++;
+                document.getElementById("score").innerHTML = `Score: ${score}`;
+                chooseKey();
+                playKeySeries();
+                console.log("yay")
+            } else {
+                checker++;
+                console.log("next");
+            }
+        } else {
+            console.log("missed");
+        }
     });
 }
+
+//When the player clicks the start button, the game begins.
+let startB = document.querySelector("#start");
+startB.addEventListener("click", () => {
+    chooseKey();
+    playKey(chosenKeys[0]);
+})
+
 
 //Need function to do the key checking?
 
