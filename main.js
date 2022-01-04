6c1d9db3e5d5532a1fa9c7b0e4b759392527d1e9
SKieronski <GitPW123!@#>
2022-01-04 09:51:36 -0600

    Added array of key objects.

--- main.js
+++ main.js
@@ -1,10 +1,44 @@
 //Default game mode will be easy
 //Initialize global variables
-let seriesN = 1;
+let seriesN = 1; //number of notes to play in series
+let gameMode = "easy"; //game difficulty
+let score = 0; //how many series have successfully been completed
+const numKeys = 5; //total number of keys
 
 //Setup array of key objects
 //Number of objects will be equal to the game mode selected
 //objects will have a key name, audio src, and randomly assigned color.
+let keyFirst = {
+    name: "C",
+    audioSrc: "",
+    color: "red"
+}
+
+let keySecond = {
+    name: "CS",
+    audioSrc: "",
+    color: "orange"
+}
+
+let keyThird = {
+    name: "D",
+    audioSrc: "",
+    color: "yellow"
+}
+
+let keyFourth = {
+    name: "DS",
+    audioSrc: "",
+    color: "green"
+}
+
+let keyFifth = {
+    name: "E",
+    audioSrc: "",
+    color: "blue"
+}
+
+let myKeys = [keyFirst, keySecond, keyThird, keyFourth, keyFifth];
 
 //Setup keys as new buttons and add to empty div? Can just start with keys in the div and manipulate those.
 
@@ -13,7 +47,10 @@ let seriesN = 1;
 //Need code logic to give the player time to press the right keys.
 //Once player presses the right keys, next series of notes will be played.
 //Is this where async timing comes into play? For giving the player a time limit.
-
+function playKey(key) {
+    let note = new Audio(key.audioSrc);
+    note.play();
+}
 //add event listeners to each key
 //check if the right key was pressed
 //when the key is pressed it plays the associated audio.
