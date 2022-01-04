7cce4bd32e398ed5924b2a2ac1e71d2a0383aa70
SKieronski <GitPW123!@#>
2022-01-04 14:53:37 -0600

    Added functionality to click events on keys. They play audio.

--- main.js
+++ main.js
@@ -10,31 +10,31 @@ const numKeys = 5; //total number of keys
 //objects will have a key name, audio src, and randomly assigned color.
 let keyFirst = {
     name: "c",
-    audioSrc: "",
+    audioSrc: "audio/c5.mp3",
     color: "red"
 }
 
 let keySecond = {
     name: "cs",
-    audioSrc: "",
+    audioSrc: "audio/c-5.mp3",
     color: "orange"
 }
 
 let keyThird = {
     name: "d",
-    audioSrc: "",
+    audioSrc: "audio/d5.mp3",
     color: "yellow"
 }
 
 let keyFourth = {
     name: "ds",
-    audioSrc: "",
+    audioSrc: "audio/d-5.mp3",
     color: "green"
 }
 
 let keyFifth = {
     name: "e",
-    audioSrc: "",
+    audioSrc: "audio/e5.mp3",
     color: "blue"
 }
 
@@ -48,13 +48,25 @@ let myKeys = [keyFirst, keySecond, keyThird, keyFourth, keyFifth];
 //Once player presses the right keys, next series of notes will be played.
 //Is this where async timing comes into play? For giving the player a time limit.
 function playKey(key) {
+    // console.log("yo");
     let note = new Audio(key.audioSrc);
     note.play();
+}
+
+function test() {
+    console.log("yo");
 }
 
 //add event listeners to each key
 //check if the right key was pressed
 //when the key is pressed it plays the associated audio.
+let docKeys = document.getElementsByClassName("keyboard");
+for(let i = 0; i < docKeys.length; i++) {
+    console.log("in for loop");
+    docKeys[i].addEventListener("click", () => {
+        playKey(myKeys[i]);
+    });
+}
 
 //Need function to do the key checking?
 
