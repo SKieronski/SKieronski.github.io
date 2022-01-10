173d0699fc613c12c7a2318bec4029b4433a73af
SKieronski <GitPW123!@#>
2022-01-09 18:12:56 -0600

    Cleaned up comments and code.

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
-//Initialize global variables
+//Setup global variables
 let score = 0; //how many series have successfully been completed
 let chosenKeys = []; //array of keys that the player will need to choose correctly to beat the game
 let checker = 0; //keeps track of where the player is at in the series
 let gameStarted = false; //used to allow player to interact with the keyboard
-const myStartSpan = document.querySelector("#start-span");
-let myScore = document.getElementById("score");
-const docKeys = document.getElementsByClassName("keyboard"); //HTML keyboard button elements in an array
-const startB = document.querySelector("#start"); //start button
-const mSheetDiv = document.getElementById("music-sheet-div"); //sheet music div
 let mSheetDivCheck = 2; //this is needed to help specify the correct mSheetDiv child node to change when the player misses.
+const myStartSpan = document.querySelector("#start-span"); 
+const myScore = document.getElementById("score"); 
+const docKeys = document.getElementsByClassName("keyboard"); 
+const startB = document.querySelector("#start"); 
+const mSheetDiv = document.getElementById("music-sheet-div"); 
 
-//Setup array of key objects
-//Number of objects will be equal to the game mode selected
-//objects will have a key name, audio src, and randomly assigned color.
+//All of our keyboard key objects.
 let keyFirst = {
     name: "c5",
     audioSrc: "audio/c5.mp3",
@@ -42,30 +40,20 @@ let keyFifth = {
     audioSrc: "audio/e5.mp3",
     color: "blue"
 }
+let myKeys = [keyFirst, keySecond, keyThird, keyFourth, keyFifth]; //array of our key objects
 
-//Array holding our key objects
-let myKeys = [keyFirst, keySecond, keyThird, keyFourth, keyFifth];
-
-
-
-//Setup keys as new buttons and add to empty div? Can just start with keys in the div and manipulate those.
-
-//add code to play a series of notes starting with 1 note
-//player will have to copy the series each time it is played
-//Need code logic to give the player time to press the right keys.
-//Once player presses the right keys, next series of notes will be played.
-//Is this where async timing comes into play? For giving the player a time limit.
+//Play the audio for a note
 function playKey(key) {
     return new Promise((resolve) => {
         let note = new Audio(key.audioSrc);
         note.volume = 0.50;
         note.play();
-        note.onended = resolve; //resolve is a function so this works with onended, resolve passes back nothing to the promise object since we just want to play the audio asynchronously
+        note.onended = resolve; 
     })
 }
 
-//Play all the keys in the chosenKeys array
-//This will need to change the color of the keys too and needs async functionality
+//Asynchronous function that plays the audio and changes the key color for 
+//all the notes in a pattern
 async function playKeySeries() {
     for(let i = 0; i < chosenKeys.length; i++) {
         changeKeyColor(chosenKeys[i]);
@@ -75,7 +63,7 @@ async function playKeySeries() {
     gameStarted = true; //once the computer plays all the keys we allow the player to play again.
 }
 
-//Change the color of a specific key document element to the key object's color value
+//Change the color of a specific key HTML element to the key object's color value
 function changeKeyColor(key) {
     for(let i = 0; i < docKeys.length; i++) {
         if(docKeys[i].id == key.name) {
@@ -84,7 +72,7 @@ function changeKeyColor(key) {
     }
 }
 
-//Clear the key color after the async function rolls through
+//Reset the color of a key once it plays its audio.
 function resetKeyColor(key) {
     for(let i = 0; i < docKeys.length; i++) {
         if(docKeys[i].id == key.name) {
@@ -97,7 +85,7 @@ function resetKeyColor(key) {
     }
 }
 
-//Add a key to the series challenge
+//Push a random key to the computer picked pattern and also add it to the hidden sheet music
 function chooseKey() {
     let rand = Math.floor(Math.random() * 5);
     chosenKeys.push(myKeys[rand]);
@@ -105,14 +93,14 @@ function chooseKey() {
     createMusicNote(chosenKeys[chosenKeys.length - 1]);
 }
 
-//update the score html and display it
+//Update the score HTML element and display it
 function displayScore() {
     myScore.innerHTML = `Score: ${score}`;
     myScore.style.opacity = 1;
     myScore.style.visibility = "visible";
 }
 
-//unhide the keyboard
+//Display the keyboard HTML element
 function displayKeyboard() {
     for (let i = 0; i < docKeys.length; i++) {
         docKeys[i].style.opacity = 1;
@@ -120,12 +108,15 @@ function displayKeyboard() {
     }
 }
 
-//Add game over screen and reset vars if the player wants to try again
+//Add game over screen
 function finalResults() {
+    //Modify the score header and start button html elements
     document.getElementById("score").innerHTML = `GAME OVER. Final Score: ${score}`;
     startB.style.opacity = 1;
     startB.style.visibility = "visible";
     myStartSpan.innerHTML = "Retry? ";
+
+    //Display sheet music
     if(chosenKeys[checker].name.charAt(1) === "-"){
         mSheetDiv.childNodes[checker + mSheetDivCheck].src = "images/music_sharp_note_green.png";
     } else {
@@ -134,34 +125,34 @@ function finalResults() {
     mSheetDiv.style.transition = "all 0.5s";
     mSheetDiv.style.opacity = 1;
     mSheetDiv.style.visibility = "visible";
-    checker = 0;
-    mSheetDivCheck = 2;
-    chosenKeys = [];
 }
 
-//add event listeners to each key
-//check if the right key was pressed
-//when the key is pressed it plays the associated audio.
+//Add event listeners to each key in the keyboard
 for(let i = 0; i < docKeys.length; i++) {
     docKeys[i].addEventListener("click", () => {
         if(gameStarted) {
             changeKeyColor(myKeys[i]);
             playKey(myKeys[i]);
-            setTimeout(resetKeyColor, 500, myKeys[i]);
-            if(myKeys[i].name === chosenKeys[checker].name) { //the player picked the right key in the series
-                if(checker === chosenKeys.length - 1) { //the player beat that series so update the score and add more keys to the challenge list
+            setTimeout(resetKeyColor, 500, myKeys[i]); 
+
+            //Game logic. 1, check if the player chose the right key
+            //If 1 is false, the player picked the wrong key and the game is over
+            //2a, check if the player picked all the keys in the current series
+            //2b, if 2a is false, let the player keep picking keys
+            if(myKeys[i].name === chosenKeys[checker].name) { 
+                if(checker === chosenKeys.length - 1) { 
                     gameStarted = false;
                     checker = 0;
                     mSheetDivCheck = 2;
                     score++;
                     displayScore();
                     chooseKey();
-                    setTimeout(playKeySeries, 1500); //start the key series after 1.5 seconds
-                } else { //the player still has more keys in the series to go
+                    setTimeout(playKeySeries, 1500); 
+                } else {
                     checker++;
                     mSheetDivCheck++;
                 }
-            } else { //the player picked the wrong key and the game is over
+            } else {
                 gameStarted = false;
                 finalResults();
             }
@@ -169,33 +160,36 @@ for(let i = 0; i < docKeys.length; i++) {
     });
 }
 
-//When the player clicks the start button, the game begins.
+//When the player clicks the start button, the game begins
 startB.addEventListener("click", () => {
+    //Reset the sheet music
     mSheetDiv.style.transition = "none";
     mSheetDiv.style.opacity = 0;
     mSheetDiv.style.visibility = "hidden";
     while(mSheetDiv.firstChild) {
         mSheetDiv.removeChild(mSheetDiv.lastChild);
     }
+
+    //Hide the start button
     startB.style.transition = "0.25s";
     startB.style.opacity = 0;
     startB.style.visibility = "hidden";
+
+    //Reset vars
+    checker = 0;
+    mSheetDivCheck = 2;
+    chosenKeys = [];
     score = 0;
+
     displayScore();
     displayKeyboard();
     createStartSheet();
+
     chooseKey();
     setTimeout(playKeySeries, 1000);
 });
 
-function createBars(){
-    let bars = document.createElement("img");
-    bars.src = "images/music_bars.png";
-    bars.style.zIndex = 1;
-    bars.style.paddingBottom = "10px";
-    mSheetDiv.appendChild(bars);
-}
-
+//Create the first element in the sheet music 
 function createStartSheet() {
     let startSheet = document.createElement("img");
     startSheet.src = "images/sheet_start.png";
@@ -204,6 +198,16 @@ function createStartSheet() {
     mSheetDiv.appendChild(startSheet);
 }
 
+//Create the bars in sheet music and add it to the sheet music div
+function createBars(){
+    let bars = document.createElement("img");
+    bars.src = "images/music_bars.png";
+    bars.style.zIndex = 1;
+    bars.style.paddingBottom = "10px";
+    mSheetDiv.appendChild(bars);
+}
+
+//Create music notes and add them to the sheet music in the appropriate position
 function createMusicNote(key) {
     let mNote = document.createElement("img");
     mNote.style.position = "absolute";
